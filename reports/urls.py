f435228a5e7df22356f477a036afdd7f0a9026be
lcorkidi <leoncorkidi@hotmail.com>
2022-04-20 17:47:52 -0500

    charge reports temp

--- reports/urls.py
+++ reports/urls.py
@@ -6,6 +6,6 @@ from .views.balance_views import BalanceView
 app_name = 'reports'
 
 urlpatterns = [
-    path('<str:fld>/charge_report/', ChargeReportView.as_view(), name='charge_report'),
+    path('<int:acc>/<str:fld>/charge_report/', ChargeReportView.as_view(), name='charge_report'),
     path('<int:lvl>/<int:ext>/<int:yr>/<int:mth>/balance/', BalanceView.as_view(), name='balance'),
 ]
