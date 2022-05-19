e7779e12f3d48b95572caeb2a6c94c5d72be6227
fisk2190 <fisk2190@gmail.com>
2022-05-19 11:24:36 -0400

    update dev

--- dev/scripts/build_docs.sh
+++ dev/scripts/build_docs.sh
@@ -9,7 +9,7 @@ python manage.py list_model_info --field-class > ${DOCSDIR}/list_model_info.txt
 echo "list_model_info.txt added to docs"
 
 echo "Creating graphs representations..."
-app_labels=('people' 'assets' 'accounting' 'documents')
+app_labels=('people' 'properties' 'reports' 'accountables' 'accounting' 'references')
 python manage.py graph_models -g \
                               -o ${DOCSDIR}/${APPNAME}_models_graph.png \
                               -E \
