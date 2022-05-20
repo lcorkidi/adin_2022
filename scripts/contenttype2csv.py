9de169d6bf7518bf4cb2116de26af29ce0a7c2f5
lcorkidi <leoncorkidi@hotmail.com>
2022-05-19 23:14:59 -0500

    backup float_format add

--- scripts/contenttype2csv.py
+++ scripts/contenttype2csv.py
@@ -1,4 +1,5 @@
 import datetime
+from numpy import NaN
 import pandas as pd
 from django.contrib.contenttypes.models import ContentType
 
@@ -8,4 +9,4 @@ def run():
     for con_typ in ContentType.objects.all():
         class_objects = con_typ.model_class().objects.all()
         if class_objects.exists():
-            pd.DataFrame(class_objects.values()).to_csv(f'_files/exports/{today.strftime("%Y-%m-%d")}_{con_typ.model}.csv')
+            pd.DataFrame(class_objects.values()).to_csv(f'_files/exports/{today.strftime("%Y-%m-%d")}_{con_typ.model}.csv', float_format='%.0f', na_rep=None)
