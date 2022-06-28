9718af8d80f45247121054102d4b1954492f3fc9
lcorkidi <leoncorkidi@hotmail.com>
2022-06-28 15:29:03 -0500

    references init e_mail refactgoring

--- references/models/__init__.py
+++ references/models/__init__.py
@@ -1,5 +1,5 @@
 from .address import Address
-from .email import E_Mail
+from .e_mail import E_Mail
 from .phone import Phone
 from .puc import PUC
 from .charge_factor import Charge_Factor, Factor_Data
